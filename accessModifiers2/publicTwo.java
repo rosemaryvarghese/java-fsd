97c15831cd2f7db9d9709ee26f08679042d450d8
Rose Mary Varghese <66167899+rosemaryvarghese@users.noreply.github.com>
2022-07-27 12:47:50 +0530

    Create publicTwo.java

---
+++ accessModifiers2/publicTwo.java
@@ -0,0 +1,9 @@
+package accessModifiers2;
+import accessModifiers.*;  
+public class publicTwo {
+	 public static void main(String args[]){  
+		   publicAcc p = new publicAcc();  
+		   p.display();  
+		  }  
+
+}
