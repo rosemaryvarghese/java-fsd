5ad1cc70dd4644ca62c8d928db124951f74fbd33
Rose Mary Varghese <66167899+rosemaryvarghese@users.noreply.github.com>
2022-07-27 11:18:10 +0530

    Update arithmeticCalc.java

--- arithmeticCalculator/arithmeticCalc.java
+++ arithmeticCalculator/arithmeticCalc.java
@@ -1,56 +1,52 @@
 package arithmeticCalculator;
-
 import java.util.Scanner;
 
-class arithmeticCalc {
+public class arithmeticCalc{
   public static void main(String[] args) {
 
     char op;
     Double n1, n2, res;
 
-    
-    Scanner in = new Scanner(System.in);
-
+   
+    Scanner s = new Scanner(System.in);
 
     System.out.println("Choose an operator: +, -, *, or /");
-    op = in.next().charAt(0);
+    op = s.next().charAt(0);
 
     System.out.println("Enter first number");
-    n1 = in.nextDouble();
+    n1 = s.nextDouble();
 
     System.out.println("Enter second number");
-    n2 = in.nextDouble();
+    n2 = s.nextDouble();
 
     switch (op) {
 
-   
+ 
       case '+':
         res = n1 + n2;
-        System.out.println(n1 + " + " + n2 + " = " + res);
+        System.out.println("SUM: "+n1 + " + " + n2 + " = " + res);
+        break;
 
-      
       case '-':
         res = n1 - n2;
-        System.out.println(n1 + " - " + n2 + " = " + res);
+        System.out.println("Difference:"+n1 + " - " + n2 + " = " + res);
         break;
 
-     
       case '*':
         res = n1 * n2;
-        System.out.println(n1 + " * " + n2 + " = " + res);
+        System.out.println("Product:"+n1 + " * " + n2 + " = " + res);
         break;
-
 
       case '/':
         res = n1 / n2;
-        System.out.println(n1 + " / " + n2 + " = " + res);
+        System.out.println("Quotient:"+n1 + " / " + n2 + " = " + res);
         break;
 
       default:
-        System.out.println("Error:Invalid Operation has been performed");
+        System.out.println("Invalid operator!");
         break;
     }
 
-    in.close();
+    s.close();
   }
 }
