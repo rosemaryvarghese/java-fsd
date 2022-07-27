68ed930cb3abe08cedd0205037b9e220deebf1da
Rose Mary Varghese <66167899+rosemaryvarghese@users.noreply.github.com>
2022-07-27 12:47:05 +0530

    Create protectedAcc.java

---
+++ accessModifiers2/protectedAcc.java
@@ -0,0 +1,10 @@
+package accessModifiers2;
+ 
+import accessModifiers.*;  
+  
+class protectedAcc extends protectedDemo{  
+  public static void main(String args[]){  
+	  protectedAcc obj = new protectedAcc();  
+   obj.dsiplay();  
+  }  
+}  
