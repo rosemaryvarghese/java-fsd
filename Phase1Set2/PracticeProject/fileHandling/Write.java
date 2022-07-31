1c2fe61d4fbcf6f100653cdea9a2b9f3cb15fd05
Rose Mary Varghese <66167899+rosemaryvarghese@users.noreply.github.com>
2022-07-31 19:48:02 +0530

    initial commit

--- Phase1Set2/PracticeProject/fileHandling/Write.java
+++ Phase1Set2/PracticeProject/fileHandling/Write.java
@@ -2,7 +2,7 @@ package fileHandling;
 
 import java.io.FileWriter;   
 
-//Importing the IOException class for handling errors  
+
 import java.io.IOException;   
 
 class Write {  
@@ -10,13 +10,13 @@ class Write {
    
  try {  
      FileWriter fwrite = new FileWriter("File.txt");  
-     // writing the content into the FileOperationExample.txt file  
+    
      fwrite.write("Java has been one of the most popular programming languages for many years.\r\n"
      		+ "Java is Object Oriented. However, it is not considered as pure object-oriented as it provides support for primitive data types (like int, char, etc)");   
 
-     // Closing the stream  
+      
      fwrite.close();   
-     System.out.println("Content is successfully wrote to the file.");  
+     System.out.println("Content has been successfully written.");  
  } catch (IOException e) {  
      System.out.println("Unexpected error occurred");  
      e.printStackTrace();  
