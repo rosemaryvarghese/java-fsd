1d8a7e67d7e09ab2b891debf00430beda32cab61
Rose Mary Varghese <66167899+rosemaryvarghese@users.noreply.github.com>
2022-07-31 19:46:31 +0530

    Update CreateFile.java

--- Phase1Set2/PracticeProject/fileHandling/CreateFile.java
+++ Phase1Set2/PracticeProject/fileHandling/CreateFile.java
@@ -1,20 +1,20 @@
 package fileHandling;
 
 import java.io.File;  
-//Importing the IOException class for handling errors  
+
 import java.io.IOException;   
 class CreateFile {  
             public static void main(String args[]) {  
             try {  
                     // Creating an object of a file  
-                    File f0 = new File("FileOperationExample.txt");   
+                    File f0 = new File("File.txt");   
                     if (f0.createNewFile()) {  
                                System.out.println("File " + f0.getName() + " is created successfully.");  
                     } else {  
-                               System.out.println("File is already exist in the directory.");  
+                               System.out.println("File already exists in the directory.");  
                     }  
                   } catch (IOException exception) {  
-                           System.out.println("An unexpected error is occurred.");  
+                           System.out.println("An unexpected error occurred.");  
                            exception.printStackTrace();  
                }   
      }  
